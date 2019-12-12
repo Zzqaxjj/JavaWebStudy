0c3bfb19b69d1e1ba5b0757607e6cf2fc9baafd9
Zzqaxjj <15711469450@163.com>
2019-12-12 20:35:37 +0800

    初始化项目

---
+++ src/test/java/com/test/array/ArrayTest.java
@@ -0,0 +1,9 @@
+package com.test.array;
+
+/**
+ * @author Zhangzhenqi
+ * @create 2019-12-12 20:08
+ */
+public class ArrayTest {
+    @Test
+}
---
+++ src/test/java/com/test/test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+/**
+ * test
+ *
+ * @author Zhangzhenqi
+ * @create 2019-12-12 20:02
+ */
+public class test {
+
+
+    @Test
+    public void test1{
+
+    }
+}
+
